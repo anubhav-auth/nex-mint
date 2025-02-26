a28009c87b0bfae50dbe94f583ad514699162f40
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-02-26 20:23:54 +0530

    changes to caro.toml

--- programs/nex-mint/src/lib.rs
+++ programs/nex-mint/src/lib.rs
@@ -1,14 +1,13 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount, MintTo, Transfer, Burn};
 
-
 declare_id!("7RbBhS3jiAeTBmWNwnXfVM2336k2hjxW2A7waT2Mw3Ph");
 
 #[program]
 pub mod nex_mint {
-
     use super::*;
 
+    // Mint new tokens to the destination account.
     pub fn mint_tokens(ctx: Context<MintTokens>, amount: u64) -> Result<()> {
         let cpi_accounts = MintTo {
             mint: ctx.accounts.mint.to_account_info(),
@@ -19,7 +18,7 @@ pub mod nex_mint {
         Ok(())
     }
 
-    // Transfer tokens between accounts.
+    // Transfer tokens from one account to another.
     pub fn transfer_tokens(ctx: Context<TransferTokens>, amount: u64) -> Result<()> {
         let cpi_accounts = Transfer {
             from: ctx.accounts.source.to_account_info(),
@@ -40,30 +39,35 @@ pub mod nex_mint {
         token::burn(CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts), amount)?;
         Ok(())
     }
-    
 }
 
 #[derive(Accounts)]
-pub struct MintTokens<'info>{
+pub struct MintTokens<'info> {
     #[account(mut)]
     pub mint: Account<'info, Mint>,
 
     #[account(mut)]
-    pub destination:Account<'info, TokenAccount>,
+    pub destination: Account<'info, TokenAccount>,
 
     #[account(signer)]
+    /// CHECK: The mint_authority is trusted to sign for minting tokens.
     pub mint_authority: AccountInfo<'info>,
-    pub token_program: Program<'info, Token>
+
+    pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
 pub struct TransferTokens<'info> {
     #[account(mut)]
     pub source: Account<'info, TokenAccount>,
+
     #[account(mut)]
     pub destination: Account<'info, TokenAccount>,
+
     #[account(signer)]
+    /// CHECK: The authority is assumed to be valid since it must sign the transaction.
     pub authority: AccountInfo<'info>,
+
     pub token_program: Program<'info, Token>,
 }
 
@@ -71,9 +75,13 @@ pub struct TransferTokens<'info> {
 pub struct BurnTokens<'info> {
     #[account(mut)]
     pub mint: Account<'info, Mint>,
+
     #[account(mut)]
     pub token_account: Account<'info, TokenAccount>,
+
     #[account(signer)]
+    /// CHECK: This authority is trusted to have the right to burn tokens.
     pub authority: AccountInfo<'info>,
+
     pub token_program: Program<'info, Token>,
 }
